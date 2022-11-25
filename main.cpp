8a81516c857b56f60d184a25f9aa5efa0b3ead63
David <d.naussed@outlook.de>
2022-11-25 17:16:32 +0100

    ECS For and Where

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 #include<tuple>
 #include<type_traits>
 #include<functional>
-
+#include<iostream>
+#include<concepts>
 #include<SDL2/SDL.h>
 
 #include"static_vector.hpp"
@@ -40,23 +41,105 @@ using ComponentArray = itlib::static_vector<T, MaxEntityCount>;
 
 template<typename... Types>
 using ComponentTable = std::tuple<ComponentArray<Types>...>;
-//
-//template<typename... types, typename currentType, typename... setTypes>
-//void set(ComponentTable<types...>& table, currentType current, setTypes... setTo);
-//
-//
-//template<typename... types, typename... setTypes>
-//void set(ComponentTable<types...>& table, setTypes... setTo) {
-//	
-//}
-//
 
+template<typename... Types>
+using ComponentSpan = std::tuple<ComponentArray<Types>*...>;
+
+template <typename T>
+struct function_traits
+	: public function_traits<decltype(&T::operator())>
+{};
+// For generic types, directly use the result of the signature of its 'operator()'
+
+template <typename ClassType, typename ReturnType, typename... Args>
+struct function_traits<ReturnType(ClassType::*)(Args...) const>
+	// we specialize for pointers to member function
+{
+	enum { arity = sizeof...(Args) };
+	// arity is the number of arguments.
+
+	typedef ReturnType result_type;
+
+	using tuple_type = std::tuple<Args...>;
+	using tuple_type_nocvref = std::tuple<std::remove_cvref_t<Args>...>;
+
+	template <size_t i>
+	struct arg
+	{
+		typedef typename std::tuple_element<i, std::tuple<Args...>>::type type;
+		// the i-th argument is equivalent to the i-th tuple element of a tuple
+		// composed of those arguments.
+	};
+};
+
+template<typename T, typename... Ts>
+struct is_member_of_type_seq { static const bool value = false; };
+
+template<typename T, typename U, typename... Ts>
+struct is_member_of_type_seq<T, U, Ts...>
+{
+	static const bool value = std::conditional<
+		std::is_same<T, U>::value,
+		std::true_type,
+		is_member_of_type_seq<T, Ts...>
+	>::type::value;
+};
+
+template<typename, typename>
+struct append_to_type_seq { };
+
+template<typename T, typename... Ts>
+struct append_to_type_seq<T, std::tuple<Ts...>>
+{
+	using type = std::tuple<Ts..., T>;
+};
+
+template<typename, typename>
+struct prepend_to_type_seq { };
+
+template<typename T, typename... Ts>
+struct prepend_to_type_seq<T, std::tuple<Ts...>>
+{
+	using type = std::tuple<T, Ts...>;
+};
+
+template<typename, typename>
+struct intersect_type_seq
+{
+	using type = std::tuple<>;
+};
+
+template<typename T, typename... Ts, typename... Us>
+struct intersect_type_seq<std::tuple<T, Ts...>, std::tuple<Us...>>
+{
+	using type = typename std::conditional<
+		!is_member_of_type_seq<T, Us...>::value,
+		typename intersect_type_seq<
+		std::tuple<Ts...>,
+		std::tuple<Us...>>
+		::type,
+		typename prepend_to_type_seq<
+		T,
+		typename intersect_type_seq<
+		std::tuple<Ts...>,
+		std::tuple<Us...>
+		>::type
+		>::type
+		>::type;
+};
 
 template<typename GetterType, typename... Types>
 ComponentArray<GetterType>& Get(ComponentTable<Types...>& table) {
 	static_assert((std::is_same_v<GetterType, Types> || ...) && "Table does not contain Type");
 	return std::get<ComponentArray<GetterType>>(table);
 }
+
+template<typename GetterType, typename... Types>
+ComponentArray<GetterType>*& Get(ComponentSpan<Types...>& table) {
+	static_assert((std::is_same_v<GetterType, Types> || ...) && "Table does not contain Type");
+	return std::get<ComponentArray<GetterType>*>(table);
+}
+
 
 template<typename SetterType, typename... Types>
 void Set(ComponentTable<Types...>& table, Entity entity, SetterType setTo) {
@@ -66,65 +149,149 @@ void Set(ComponentTable<Types...>& table, Entity entity, SetterType setTo) {
 }
 
 template<typename... Types>
+constexpr size_t Size(const ComponentTable<Types...>& table) {
+	return std::get<0>(table).size(); // If this fails you do not have a table
+}
+
+template<typename... Types>
+constexpr size_t Size(const ComponentSpan<Types...>& span) {
+	return std::get<0>(span)->size(); // If this fails you do not have a table
+}
+
+template<typename... Types>
 Entity Create(ComponentTable<Types...>& table, std::tuple<Types...> data) {
 	(std::get<ComponentArray<Types>>(table).push_back(std::get<Types>(data)), ...);
-	return table._Myfirst._Val.size() - 1;
+
+	return Size(table) - 1;
 }
 
-template<typename... Types, size_t size>
-void ForEach(ComponentTable<Types...>& table, EntityRange<size> entityRange, std::function<void(Types...)> func) {
-	const size_t max = table._Myfirst._Val.size();
-	using TypesPlain = std::tuple<std::remove_reference_t<std::remove_const_t<Types>>...>;
-
-	for (size_t entity = 0; entity < max, entity += 1) {
-		
-	}
+template<typename... Types>
+Entity Create(ComponentTable<Types...>& table) {
+	const std::tuple<Types...> tuple { };
+	(std::get<ComponentArray<Types>>(table).push_back(tuple), ...);
+	return Size(table) - 1;
 }
 
-
-template<size_t size, typename... Types>
-void Test(EntityRange<size> entityRange, std::function<void(Types...)> tests) {
-
-}
-
-//template<typename... types>
-//class ComponentTable {
-//public:
+//template<size_t size, typename... Types>
+//void For(ComponentTable<Types...>& table, EntityRange<size> entityRange, std::function<void(Types...)> func) {
+//	const size_t max = Size(table);
 //
-//private:
-//	std::tuple<ComponentArray<types>...> data{ };
-//};
+//	for (size_t entity = 0; entity < max; entity += 1) {
+//		(func(std::get<ComponentArray<std::remove_cvref_t<Types>>>(table)[entity]...));
+//	}
+//}
+
+template<typename... Types, typename Func> 
+constexpr void For(ComponentTable<Types...>& table, Func func) {
+	static_assert(sizeof...(Types) > 0, "No data in table");
+
+	using traits = function_traits<Func>;
+	using A = traits::tuple_type_nocvref;
+	using B = std::tuple<Types...>;
+	using Result = intersect_type_seq<A, B>::type;
+	constexpr size_t size = std::tuple_size_v<Result>;
+
+	static_assert(size > 0, "No Valid parameters in Function");
+
+	[&] <size_t... Is>(auto&& t, std::index_sequence<Is...>) {
+		const auto max = Size(table);
+
+		for (size_t index = 0; index < max; index += 1) {
+			func(std::get<ComponentArray<std::remove_cvref_t<std::tuple_element<Is, typename traits::tuple_type>::type>>>(t)[index]...);
+		}
+	} (table, std::make_index_sequence<traits::arity>{});
+}
+
+template<typename... Types, typename Func>
+constexpr void For(ComponentSpan<Types...>& table, Func func) {
+	static_assert(sizeof...(Types) > 0, "No data in table");
+
+	using traits = function_traits<Func>;
+	using A = traits::tuple_type_nocvref;
+	using B = std::tuple<Types...>;
+	using Result = intersect_type_seq<A, B>::type;
+	constexpr size_t size = std::tuple_size_v<Result>;
+
+	static_assert(size > 0, "No Valid parameters in Function");
+
+	[&] <size_t... Is>(auto && t, std::index_sequence<Is...>) {
+		const auto max = Size(table);
+
+		for (size_t index = 0; index < max; index += 1) {
+			func((*std::get<ComponentArray<std::remove_cvref_t<std::tuple_element<Is, typename traits::tuple_type>::type>>*>(t))[index]...);
+		}
+	} (table, std::make_index_sequence<traits::arity>{});
+}
+
+//
+//template<const size_t size, typename... Types, typename Func>
+//constexpr void For(EntityRange<size> entityContainer, ComponentTable<Types...>& table, Func func) {
+//	static_assert(sizeof...(Types) > 0, "No data in table");
+//
+//	using traits = function_traits<Func>;
+//	using A = traits::tuple_type_nocvref;
+//	using B = std::tuple<Types...>;
+//	using Result = intersect_type_seq<A, B>::type;
+//	constexpr size_t size = std::tuple_size_v<Result>;
+//
+//	static_assert(size > 0, "No Valid parameters in Function");
+//
+//	[&] <size_t... Is>(auto && t, std::index_sequence<Is...>) {
+//		for (const auto& entity : entityContainer) {
+//			func(std::get<ComponentArray<std::remove_cvref_t<std::tuple_element<Is, typename traits::tuple_type>::type>>>(t)[entity]...);
+//		}
+//
+//	} (table, std::make_index_sequence<traits::arity>{});
+//}
+
+template<typename... WhereTypes, typename... Types>
+constexpr auto Where(ComponentTable<Types...>& table) -> ComponentSpan<WhereTypes...> {
+	//std::tuple<ComponentArray<WhereTypes>*...> returnTuple;
+	//(std::get<ComponentArray<WhereTypes>*>(returnTuple) = std::get<ComponentArray<WhereTypes>>(table), ...);
+	return ComponentSpan<WhereTypes...>{ &std::get<ComponentArray<WhereTypes>>(table)... };
+	//return std::tuple(&std::get<ComponentArray<WhereTypes>>(table)...);
+}
 
 #pragma region Main
 
-static ComponentTable<int, float> masterTable;
+struct Speed {
+	float value;
+};
+
+struct Position {
+	float x;
+	float y;
+};
+
+struct Color {
+	SDL_Color value;
+};
+
+struct Velocity {
+	float x;
+	float y;
+};
+
+static ComponentTable<Position, Speed, Velocity, Color> masterTable;
 
 int main(int, char**) {
-	//PushBack(std::get<ComponentArray<int>>(masterTable));
-	Entity player = Create(masterTable, { 0, 0.0f});
-	Set(masterTable, player, 5);
-	auto& intColumn = Get<int>(masterTable);
-	auto& floatColumn = Get<float>(masterTable);
-	for (auto& bullshit : intColumn) {
-		// Do stuff on each int
-	}
-
-	for (auto& bullshit : floatColumn) {
-		// Do stuff on each float
-	}
-
-	EntityRange<3> range = {0, 1, 2 };
-
-	std::function<void(int, float)> testy;
-	ForEach(masterTable, range, testy);
-
-	std::tuple<int, float, char, size_t> test;
-	std::tuple<int, char> other;
-	auto lhs = std::tuple_cat(test, other);
-	std::tie(std::get<int>(test), std::get<int>(test), std::ignore, std::get<int>(test)) = test;
-
 	SDL_Window* window = SDL_CreateWindow("Basic Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 640, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	// Just in case we need player later, let's store it : ) 
+	Entity player = Create(masterTable, { });
+	Create(masterTable, { });
+	Create(masterTable, { });
+	Create(masterTable, { });
+	Create(masterTable, { });
+
+	auto physicsTable = Where<Position, Speed, Velocity>(masterTable);
+	auto renderTable = Where<Position, Color>(masterTable);
+
+	// Setup colours
+	For(masterTable, [](Color& color) {
+		color.value = {255, 255, 255, 255};
+	});
 	
 	auto quitWatcher = [](void* data, SDL_Event* e) { 
 		if (e->type == SDL_QUIT) {
@@ -141,7 +308,26 @@ int main(int, char**) {
 
 		}
 
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
+
+		For(physicsTable, [](Speed speed, Velocity& vel) {
+			vel.x += speed.value;
+			vel.y += speed.value;
+			});
+
+
+		For(physicsTable, [](Position& pos, Velocity vel) {
+			pos.x += vel.x;
+			pos.y += vel.y;
+			});
+
+		For(renderTable, [&renderer](Position pos, Color color) {
+			SDL_FRect rect{ pos.x, pos.y, 32.0f, 32.0f };
+			auto c = color.value;
+			SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, c.a);
+			SDL_RenderFillRectF(renderer, &rect);
+			});
 
 		// Drawing here
 
